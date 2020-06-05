69f553635b6fce0076c1c5582db74048a4640982
daviromao <davi.romao0210@gmail.com>
2020-06-05 11:46:24 -0300

    feat: add daily data and change covid  api

--- src/config.js
+++ src/config.js
@@ -1,8 +1,18 @@
-exports.model = `
+exports.tweetModel = `
 🦠 Atualizações da COVID-19 no Brasil - {date} 🦠
 
-😷 Casos Confirmados: {cases}
-✝️ Total de Mortos: {deaths}
+✅ Novos casos : {newcases}
+💔 Novas mortes : {newdeaths}
+
+😷 Total de casos confirmados: {cases}
+✝️ Total de mortos: {deaths}
 
 #COVID19BR #COVID #CORONAVIRUS #COVID19
 `;
+
+exports.logModel = `
+- New status -
+Date: {date}
+ID: {id}
+--------------
+`
